1bcf3c83c96fc042a03e7660e721eb9e6120ae43
Hideaki Kondo <hdaknd+github@gmail.com>
2021-04-19 01:28:58 +0900

    add youtube api client

--- server.go
+++ server.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"github.com/hideakikondo/youtube-manager-go/routes"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Fatal("Error loadinf .env")
+	}
+
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
 func main() {
-    e := echo.New()
+	e := echo.New()
 
-    // Middlewares
+	// Middlewares
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
 
-    // Routes
-    routes.Init(e)
+	// Routes
+	routes.Init(e)
 
-    // Start server
-    e.Logger.Fatal(e.Start(":8080"))
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
